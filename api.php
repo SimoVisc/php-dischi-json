acfa8562afb4b69f47c5243825633a539af41b38
SimoVisc <simonaviscardivs@gmail.com>
2022-12-19 15:46:19 +0100

    Aggiungo il filtro sui generi

--- api.php
+++ api.php
@@ -2,5 +2,11 @@
 <?php
 $albums = file_get_contents("dischi.json");
 
+if(!empty($_GET['genre'])) {
+    $albums = json_decode($albums, true);
+    $albums = array_filter($albums, fn($elm) => strtolower($elm['genre']) === strtolower($_GET['genre']) );
+    $albums = json_encode($albums);
+}
+
 header("Content-Type: application/json");
 echo $albums;
--- js/main.js
+++ js/main.js
@@ -4,11 +4,26 @@ createApp({
   data() {
     return {
       albums: [],
+      genre: "",
     };
   },
   created() {
     axios.get("http://localhost/php-dischi-json/api.php").then((res) => {
       this.albums = res.data;
     });
+  },
+  methods: {
+    onSelectChange() {
+      console.log(this.genre);
+      axios
+        .get("http://localhost/php-dischi-json/api.php", {
+          params: {
+            genre: this.genre,
+          },
+        })
+        .then((res) => {
+          this.albums = res.data;
+        });
+    },
   },
 }).mount("#app");
